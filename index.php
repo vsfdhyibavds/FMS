749691e51b5965d7ad7d3ee49474ba275fbc014e
vsfdhyibavds <eugenco578@gmail.com>
2024-06-27 16:59:45 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+include("db_connect.php");
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = mysqli_real_escape_string($conn, $_POST['username']);
+    $password = mysqli_real_escape_string($conn, $_POST['password']);
+
+    $query = mysqli_query($conn, "SELECT * FROM user WHERE username = '$username' AND password = '$password'") or die(mysqli_error($conn));
+    $row = mysqli_fetch_array($query);
+
+    if (mysqli_num_rows($query) == 1) {
+        $_SESSION['id'] = $row['user_id'];
+        header("Location: admin_page.php");
+    } else {
+        echo "Invalid username or password.";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <h1>Login</h1>
+    <form method="post" action="">
+        <div>
+            <label>Username</label>
+            <input type="text" name="username" required>
+        </div>
+        <div>
+            <label>Password</label>
+            <input type="password" name="password" required>
+        </div>
+        <button type="submit">Login</button>
+    </form>
+</body>
+</html>
---
+++ user_page.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User page</title>
+
+    <!-- custom css file link -->
+     <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+    <div class="container">
+        <div class="content">
+            <h3>hi, <span>user</span></h3>
+            <h1>welcome <span></span></h1>
+            <p>this is a user page</p>
+            <a href="login_form.php" class="btn">login</a>
+            <a href="register_form.php" class="btn">register</a>
+            <a href="logout_form.php" class="btn">logout</a>
+        </div>
+    </div>
+</body>
+</html>
